a9b1bb824867f102540f3a67c681ef257d08110b
Ryan Stocks <ryan.stocks00@gmail.com>
2022-05-11 22:32:05 +1000

    Prettier plots

--- src/positron_cross_section/plot.py
+++ src/positron_cross_section/plot.py
@@ -16,7 +16,7 @@ plt.rcParams["font.serif"] = ["Times New Roman"]
 
 def cross_section_plot() -> Any:
     """Create figure and axes for a cross section plot."""
-    fig, ax = plt.subplots(figsize=(7, 6))
+    fig, ax = plt.subplots(figsize=(6, 5))
     # ax.set_yscale("log")
     ax.set_xscale("log")
     ax.set(xlabel="Incident energy (eV)", ylabel="Cross section $\\sigma\\ \\ (Å^2)$")
